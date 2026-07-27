85f5cbc125b3019c80404c4ddf33da876998082e
Tonir <tonirshaik@gmail.com>
2026-07-27 17:21:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -251,4 +251,4 @@ def add_account():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True) 
